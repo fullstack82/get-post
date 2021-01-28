aed316c01b0b7469e6025c76a323cc65cbe6a11f
fullstack82 <fullstack8210@gmail.com>
2021-01-28 11:42:10 +0100

    creando formulario de php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" 7>
+    <title>Formulario en PHP</title>
+</head>
+
+<body>
+    <h1>Formulario en PHP</h1>
+    <form method="GET" action="recibir.php">
+        <p>
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre">
+        </p>
+        <p>
+            <label for="apellidos">Apellidos</label>
+            <input type="text" name="apellidos">
+        </p>
+        <label for="email">Email</label>
+        <input type="text" name="email">
+
+        <input type="submit" value="Enviar datos"/>
+    </form>
+
+
+
+</body>
+
+</html>
